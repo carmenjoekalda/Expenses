4b3f32e09d6d5b2ecdf240ff1217a9610b59d0c8
carmenjoekalda <cjoekalda@gmail.com>
2023-10-16 21:02:46 +0300

    added a button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,20 @@ import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js"
 
 const ExpenseItem = (props) => {
-	return(
+	const clickHandler = () => {
+		console.log('Clicked!')
+	}
+
+	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.expenseData.date}></ExpenseDate>
 			<div className="expense-item__description">
 				<h2>{props.expenseData.title}</h2>
 				<div className="expense-item__price">{props.expenseData.price}</div>
 			</div>
+			<button onClick={clickHandler}>Change Title</button> 
 		</Card>
 	)
 }
 
-export default ExpenseItem;
+export default ExpenseItem
